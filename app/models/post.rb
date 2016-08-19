847de55eba57da308ee6c98a3185b610be2e938f
kw7oe <choongkwern@hotmail.com>
2016-08-19 18:21:50 +0800

    Add in acts_as_votable and Styling for Post

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,6 @@
 class Post < ActiveRecord::Base
-	has_attached_file :image, styles: { medium: "400x400" }, default_url: "/images/:style/missing.png"
+	acts_as_votable
+	has_attached_file :image, styles: { medium: "400x400#" }, default_url: "/images/:style/missing.png"
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
   belongs_to :user
 end
