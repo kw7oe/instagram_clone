6962927bd3bab158257cffdc08ca166195397173
kw7oe <choongkwern@hotmail.com>
2016-08-21 15:32:41 +0800

    Add in User Followers and Following Views

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,30 @@
 class UsersController < ApplicationController
+	before_action :set_user
 
 	def show 
+		@posts = @user.posts.order("created_at DESC")
+	end
+
+	def followers
+		@users = @user.followers
+		if @users.empty?
+			redirect_to @user
+		else 
+			render 'show_follow'
+		end
+	end
+
+	def following
+		@users = @user.following
+		if @users.empty?
+			redirect_to @user
+		else 			
+			render 'show_follow'
+		end
+	end
+
+	private 
+	def set_user 
 		@user = User.find(params[:id])
 	end
 	
