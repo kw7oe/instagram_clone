4620ff30efafa83469d53552546f688fc326d9d9
kw7oe <choongkwern@hotmail.com>
2016-08-21 23:42:49 +0800

    Work on user feeds

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,9 @@
 class StaticPagesController < ApplicationController
-  def home
+  def home  	
+	  @feeds = []
+  	if current_user
+	  	@feeds = Post.where("user_id IN (?) OR user_id = ?", current_user.following_ids, current_user.id)
+	  	@feeds = @feeds.order('created_at DESC')
+	  end
   end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+
+	def time_ago(time)
+		string = time_ago_in_words(time)
+		string = string.split(" ")
+		string.shift if string[0] !~ /\d/
+		string = string[0] + string[1][0]
+		return string
+	end
 end
